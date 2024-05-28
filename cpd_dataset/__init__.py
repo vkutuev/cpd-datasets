a5a6d40a941a7059eff3d6247d9b6b69514a3711
Vladimir Kutuev <vladimir.kutuev@gmail.com>
2024-05-28 19:37:27 +0300

    feat: convert cpd_dataset to executable package

---
+++ cpd_dataset/__init__.py
@@ -0,0 +1,8 @@
+"""
+Datasets generator for Change Point Detection problem
+=====================================================
+"""
+
+__copyright__ = "Copyright (c) 2024 Vladimir Kutuev"
+
+__license__ = "SPDX-License-Identifier: MIT"
---
+++ cpd_dataset/__main__.py
@@ -0,0 +1,12 @@
+"""
+Main entry point for Change Point Detection problem dataset generator.
+"""
+
+__copyright__ = "Copyright (c) 2024 Vladimir Kutuev"
+
+__license__ = "SPDX-License-Identifier: MIT"
+
+from .main import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
